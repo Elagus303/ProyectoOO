262e6df9d2753d850b8d5aecb4100ae0c4626152
AlexRubenPumari <alexpumari0@gmail.com>
2024-09-26 15:09:26 -0300

    Algun mensaje

--- ProyectoOO/Ventas.vb
+++ ProyectoOO/Ventas.vb
@@ -10,6 +10,6 @@
     Private Sub Ventas_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'TODO: esta línea de código carga datos en la tabla 'BD_ImprentaDataSet.Venta' Puede moverla o quitarla según sea necesario.
         Me.VentaTableAdapter.Fill(Me.BD_ImprentaDataSet.Venta)
-
+        'ccececececec monton de codigo
     End Sub
 End Class
